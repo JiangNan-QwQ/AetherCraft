ac8025c7d8ef01b2822b6527a707d63eacf1f61b
JiangNan-QwQ <1706491377@qq.com>
2025-06-15 00:44:30 +0800

    73635

--- 运行此脚本.sh
+++ 运行此脚本.sh
@@ -18,5 +18,5 @@ if check_termux; then
 pkg install -y curl ; bash -c "$(https://raw.githubusercontent.com/JiangNan-QwQ/AetherCraft/main/容器.sh)"
 else
     echo "检测到运行环境: 标准Linux系统"
-pkg install -y curl ; bash -c "$(https://raw.githubusercontent.com/JiangNan-QwQ/AetherCraft/main/3.3.sh)"
+apt install -y curl && bash -c "$(https://raw.githubusercontent.com/JiangNan-QwQ/AetherCraft/main/3.3.sh)"
    fi
