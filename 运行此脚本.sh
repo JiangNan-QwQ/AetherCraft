f00779d3f1c7b81326b287dc77b4faecbb810c94
JiangNan-QwQ <1706491377@qq.com>
2025-06-14 23:48:41 +0800

    446

--- 运行此脚本.sh
+++ 运行此脚本.sh
@@ -1,18 +1,5 @@
 #启动脚本
 
-
-# 获取脚本的绝对路径（包括脚本名）
-SCRIPT_PATH="$(realpath "$0")"
-
-
-YUANWEI_ZHI="$(dirname "$SCRIPT_PATH")"
-
-UBUNTU_ROOT=/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root
-
-MARKER_FILE="$HOME/.jack_eason"
-
-#!/bin/bash
-
 # 检测Termux环境
 check_termux() {
     if [ -d "/data/data/com.termux/files/usr" ] && [ -n "$TERMUX_VERSION" ]; then
@@ -25,20 +12,11 @@ check_termux() {
 # 主逻辑
 if check_termux; then
     echo "检测到运行环境: Termux"
-
-if [ ! -f "$MARKER_FILE" ]; then
-    echo "首次运行脚本！"
-    echo "换源。。。"
-    sleep 1
-    bash $YUANWEI_ZHI/换源.sh
-    touch "$MARKER_FILE"  # 创建标记文件
-else
     echo "欢迎，即将进入"
     sleep 1
-fi
 
-bash $YUANWEI_ZHI/容器.sh
+pkg install -y curl ; bash -c "$(https://raw.githubusercontent.com/JiangNan-QwQ/AetherCraft/main/容器.sh)"
 else
     echo "检测到运行环境: 标准Linux系统"
-   bash $YUANWEI_ZHI/3.3.sh
-fi
+pkg install -y curl ; bash -c "$(https://raw.githubusercontent.com/JiangNan-QwQ/AetherCraft/main/3.3.sh)"
+   fi
